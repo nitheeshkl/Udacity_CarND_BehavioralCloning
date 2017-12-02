00618386e5d9b4e19e3f23f0d0182ea081d940c9
Nitheesh K L <nitheesh.k.l@intel.com>
2017-12-03 03:10:51 +0530

    implement basic cnn for training
    Signed-off-by: Nitheesh K L <nitheesh.k.l@intel.com>

--- train.py
+++ train.py
@@ -32,13 +32,21 @@ Y_train = np.array(steering_measurements)
 # build a simple regression network for testing data
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=X_train[0].shape))
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss="mse", optimizer="adam")
-model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
+model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
-model.save("./model.h5")
+model.save("./model_lenet.h5")
